5fa78e68ff88203ff577c76a4d55fa0497f3d882
patrick.gdbt <patrickcgodbout@gmail.com>
2021-10-14 16:58:00 -0400

    continued part 1, again

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score
 
+##Create distribution graph of classes
+
 businessI = len([name for name in os.listdir("./BBC/business") if os.path.isfile(os.path.join("./BBC/business", name))])
 entertainmentI = len(
     [name for name in os.listdir("./BBC/entertainment") if os.path.isfile(os.path.join("./BBC/entertainment", name))])
@@ -23,23 +25,51 @@ plt.title('BBC distribution')
 plt.bar(names, values)
 plt.savefig('BBC-distribution.pdf')
 
+##Load corpus
+
 corpus = datasets.load_files('./BBC', encoding='latin1')
+
+##Pre process
 
 x = corpus.data
 y = corpus.target
 vec = CountVectorizer()
 matrix = vec.fit_transform(x)
 
+##Train test
+
 trainX, testX, trainY, testY = train_test_split(matrix, y, train_size=0.8, test_size=0.2, random_state=None)
+
+f = open("bbc-performance.txt", "w")
+f.write("###################################################################\n")
+f.write("               Multinomial default values, try 1\n")
+f.write("###################################################################\n")
+
 clf = MultinomialNB().fit(trainX, trainY)
 
 predict = clf.predict(testX)
-cm = confusion_matrix(testY, predict)
-cr = classification_report(testY, predict, target_names=names)
-acc = accuracy_score(testY, predict)
-f1 = f1_score(testY, predict, average=None)
 
-# do prior probability on test set (?)
+cm = confusion_matrix(testY, predict)
+
+f.write("(b): confusion matrix\n")
+f.write(str(cm))
+f.write("\n")
+
+cr = classification_report(testY, predict, target_names=names)
+
+f.write("(c): classification report\n")
+f.write(str(cr))
+f.write("\n")
+
+acc = accuracy_score(testY, predict)
+macrof1 = f1_score(testY, predict, average='macro')
+weightedf1 = f1_score(testY, predict, average='weighted')
+
+f.write("(d): accuracy, macro f1, weighted f1\n")
+f.write("accuracy: " + str(acc) + "\n")
+f.write("macro f1: " + str(macrof1) + "\n")
+f.write("weighted f1: " + str(weightedf1) + "\n")
+
 businessN = 0
 entertainmentN = 0
 politicsN = 0
@@ -65,15 +95,18 @@ politicsPercentage = "{:.2%}".format(politicsN / total)
 sportPercentage = "{:.2%}".format(sportN / total)
 techPercentage = "{:.2%}".format(techN / total)
 
-print("Prior Probability:")
-print("Business: " + businessPercentage)
-print("Entertainment: " + entertainmentPercentage)
-print("Politics: " + politicsPercentage)
-print("Sport: " + sportPercentage)
-print("Tech: " + techPercentage)
+f.write("(e): prior probabilities\n")
+f.write("Business: " + businessPercentage + " \n")
+f.write("Entertainment: " + entertainmentPercentage + " \n")
+f.write("Politics: " + politicsPercentage + " \n")
+f.write("Sport: " + sportPercentage + " \n")
+f.write("Tech: " + techPercentage + " \n")
+
+f.write("(f): size of vocabulary \n")
 
 vocabulary = len(vec.vocabulary_)
-print(vocabulary)
+
+f.write("Vocabulary size: " + str(vocabulary) + "\n")
 
 businessWordTokens = matrix[y == 0].sum()
 entertainmentWordTokens = matrix[y == 1].sum()
@@ -81,6 +114,12 @@ politicsWordTokens = matrix[y == 2].sum()
 sportWordTokens = matrix[y == 3].sum()
 techWordTokens = matrix[y == 4].sum()
 
+f.write("(g): number of word tokens in each class")
+f.write("Business: " + str(businessWordTokens) + " \n")
+f.write("Entertainment: " + str(entertainmentWordTokens) + " \n")
+f.write("Politics: " + str(politicsWordTokens) + " \n")
+f.write("Sport: " + str(sportWordTokens) + " \n")
+f.write("Tech: " + str(techWordTokens) + " \n")
 print(businessWordTokens)
 print(entertainmentWordTokens)
 print(politicsWordTokens)
@@ -91,4 +130,16 @@ entireCorpusWordTokens = businessWordTokens + entertainmentWordTokens + politics
 
 print(entireCorpusWordTokens)
 
+f.write("(h): number of word tokens in entire corpus\n")
+f.write("Number of word tokens in corpus: " + str(entireCorpusWordTokens) + "\n")
+
+
+##TODO i, j, k, 8, 9, 10, 11
 count = 0
+
+test = matrix[y == 0]
+for word in matrix[y == 4].data:
+    if word == 0:
+        count += 1
+
+print(count, "pp")
