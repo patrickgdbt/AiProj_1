a1143a5e783e5d80761c9b8de67dc523bc7eec08
William <williammongeau@gmail.com>
2021-10-14 16:08:43 -0400

    Part 2 mostly done except for "TOP" models

--- main2.py
+++ main2.py
@@ -1,8 +1,12 @@
+import statistics
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score, f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.neural_network import MLPClassifier
 
 # Load the CSV into a Pandas Data Frame
 df = pd.read_csv('drug200.csv')
@@ -45,20 +49,49 @@ df2['Cholesterol'] = df2['Cholesterol'].cat.codes
 y = df2['Drug']
 x = df2.drop(['Drug'], axis=1)
 train_x, test_x, train_y, test_y = train_test_split(x, y)
-#
-# # print(train_x)
 
-clf = MultinomialNB().fit(train_x, train_y)
+models = [ MultinomialNB(), DecisionTreeClassifier(), Perceptron(), MLPClassifier() ]
 
-predict = clf.predict(test_x)
-cm = confusion_matrix(test_y, predict, labels=categories)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=categories)
-disp.plot()
-# ConfusionMatrixDisplay.from_estimator()
-# sklearn.metrics.plot_confusion_matrix(test_y, predict, labels=categories)
-plt.show()
-cr = classification_report(test_y, predict)
-acc = accuracy_score(test_y, predict)
-f1 = f1_score(test_y, predict, average=None)
+f = open("drugs-performance.txt", "w")
+f.write("Step 7:\n")
 
-print(cm)
+for model in models:
+    f.write("--------------------------------------------\n")
+    f.write("a) "+str(type(model))+" with default parameters\n")
+    model.fit(train_x, train_y)
+    predict = model.predict(test_x)
+    cm = confusion_matrix(test_y, predict, labels=categories)
+    f.write("b) Confusion Matrix:\n ")
+    f.write(str(cm)+"\n")
+    cr = classification_report(test_y, predict)
+    f.write("c)\n")
+    f.write(str(cr))
+    f.write("d)\n")
+    acc = accuracy_score(test_y, predict)
+    f.write("Accuracy: " + str(acc)+"\n")
+    f1Macro = f1_score(test_y, predict, average='macro')
+    f.write("F1 Macro Average: " + str(f1Macro)+"\n")
+    f1Weighted = f1_score(test_y, predict, average='weighted')
+    f.write("F1 Weighted Average: " + str(f1Weighted)+"\n")
+
+f.write("Step 8:\n")
+for model in models:
+    accuracy = []
+    f1Macro = []
+    f1Weighted = []
+    for i in range(10):
+        model.fit(train_x, train_y)
+        predict = model.predict(test_x)
+        accuracy.append(accuracy_score(test_y, predict))
+        f1Macro.append(f1_score(test_y, predict, average='macro'))
+        f1Weighted.append(f1_score(test_y, predict, average='weighted'))
+    f.write("-------------------------------------------\n")
+    f.write(str(type(model))+" with default parameters\n")
+    f.write("Average Accuracy: " + str(statistics.mean(accuracy))+"\n")
+    f.write("Average F1 Macro Average: " + str(statistics.mean(f1Macro))+"\n")
+    f.write("Average F1 Weighted Average: " + str(statistics.mean(f1Weighted))+"\n")
+    f.write("Standard Deviation Accuracy: " + str(statistics.stdev(accuracy))+"\n")
+    f.write("Standard Deviation F1 Macro Average: " + str(statistics.stdev(accuracy))+"\n")
+    f.write("Standard Deviation F1 Weighted Average: " + str(statistics.stdev(accuracy))+"\n")
+
+f.close()
