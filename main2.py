a509137ecbbcd94d9f3df88e71f67f2b474e398f
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2021-10-14 14:17:19 -0400

    ckpnt

--- main2.py
+++ main2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score, f1_score
 
 # Load the CSV into a Pandas Data Frame
 df = pd.read_csv('drug200.csv')
@@ -26,12 +28,37 @@ but the column is "Category" and not just a string (for BP and Chol) and we can 
 
 If we want to go from category to int theres also better ways to do it that the assignment doesn't reference.
 """
+categories = ['drugA', 'drugB', 'drugC', 'drugX', 'drugY']
 # df['Drug'] = pd.Categorical(df['Drug'], categories=['drugA', 'drugB', 'drugC', 'drugX', 'drugY'])
 df2 = pd.get_dummies(df, columns=['Sex'])
 
 df2['BP'] = pd.Categorical(df2['BP'], categories=['LOW', 'NORMAL', 'HIGH'])
+df2['BP'] = df2['BP'].cat.codes
+
 df2['Cholesterol'] = pd.Categorical(df2['Cholesterol'], categories=['LOW', 'NORMAL', 'HIGH'])
+df2['Cholesterol'] = df2['Cholesterol'].cat.codes
+
 # Uncomment if we want to assign class to category (unsure).
 # df['Drug'] = pd.Categorical(df['Drug'], categories=['drugA', 'drugB', 'drugC', 'drugX', 'drugY'])
 
-train, test = train_test_split(df2)
+# x, y = df2.copy().drop(['Drug']), df2['Drug']
+y = df2['Drug']
+x = df2.drop(['Drug'], axis=1)
+train_x, test_x, train_y, test_y = train_test_split(x, y)
+#
+# # print(train_x)
+
+clf = MultinomialNB().fit(train_x, train_y)
+
+predict = clf.predict(test_x)
+cm = confusion_matrix(test_y, predict, labels=categories)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=categories)
+disp.plot()
+# ConfusionMatrixDisplay.from_estimator()
+# sklearn.metrics.plot_confusion_matrix(test_y, predict, labels=categories)
+plt.show()
+cr = classification_report(test_y, predict)
+acc = accuracy_score(test_y, predict)
+f1 = f1_score(test_y, predict, average=None)
+
+print(cm)
